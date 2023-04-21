f3dd5bd70dcf8e8a8c99bd7a8db5c1eb7848f6c6
Willian Gonçalves Rios <71411461+williangrios@users.noreply.github.com>
2023-04-21 12:18:35 -0300

    update

--- frontend/hooks/useConnect.js
+++ frontend/hooks/useConnect.js
@@ -6,6 +6,6 @@ export const useConnect = async (provider) => {
     const web3 = new Web3(window.ethereum);
     const accounts = await web3.eth.requestAccounts();
     if (!accounts || !accounts.length)
-      throw new Error("Wallet not found/allowed");
+      throw new Error("Wallet not found/allowed.");
     return accounts[0]
-}
+} 
